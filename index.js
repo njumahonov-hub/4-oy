f19ef276ee2f48ab5de2e2eb728992c7f1715bb1
njumahonov-hub <njumahonov@gmail.com>
2025-11-16 14:47:00 +0500

    2-work

---
+++ index.js
@@ -0,0 +1,50 @@
+// 1-masala
+
+// const {add, minus} = require("./main")
+
+// console.log(add(2,2));
+// console.log(minus(6,4));
+
+// 2-masala
+
+// const fs = require("fs")
+
+// const data =fs.readFileSync("main.js", "utf-8")
+// console.log(data);
+
+// const fs = require("fs")
+
+// const data = fs.writeFileSync("test.txt", "salom dunyo!")
+
+// 3-masala
+
+// const fs = require("fs")
+
+
+// const arr= [2,5,7,2,3,8,9,2,6,8]
+
+// const buffer = Buffer.from(arr)
+// const data = fs.writeFileSync("arr.bin", buffer)
+
+// const buffer =  fs.readFileSync("arr.bin")
+// const arr = Array.from(buffer)
+// console.log(arr.join(" "));
+
+
+
+// 4-masala
+
+// const fs  = require("fs")
+
+// fs.writeFileSync("tsconfig.json", "")
+// fs.writeFileSync("README.md", "")
+// fs.writeFileSync("main.go", "")
+// fs.writeFileSync("golang-todo-app", "")
+// fs.writeFileSync("go.sum", "")
+// fs.writeFileSync("go.mod", "")
+// fs.mkdirSync("static")
+// fs.appendFileSync("static/stlye.css", "")
+// fs.appendFileSync("static/script.ts", "")
+// fs.appendFileSync("static/script.js", "")
+// fs.mkdirSync("html")
+// fs.appendFileSync("html/index.html", "")
---
+++ main.js
@@ -0,0 +1,13 @@
+// 1- masala
+
+// function add(a,b) {
+//     return a + b
+// }
+// function minus(a,b) {
+//     return a - b
+// }
+
+// module.exports = {
+//     add,
+//     minus
+// }
